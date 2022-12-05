2db772fc51772467c9bc27bf1394e35f8fc943b9
Boe <boe3am@gmail.com>
2022-12-05 23:46:43 +0100

    listings API stylechange

--- src/js/templates/renderListings.mjs
+++ src/js/templates/renderListings.mjs
@@ -5,26 +5,26 @@ export function renderListings(listings) {
 
   for (let i = 0; i < listings.length; i++) {
     const listing = listings[i];
-    if (i === 12) break;
+    if (i === 16) break;
 
     const image = listing.media
       ? `<img 
       src="${listing.media}"
       alt="Image for ${listing.title}"
-      class="auction-image"
+      class="auction-image img-fluid"
       />`
       : "";
 
     container.innerHTML += `
-        <a href="listing/?id=${listing.id}">
-        <div class="card m-2 single-listing">
-        <h2 class="listing-title m-2">${listing.title}</h2>
+        <div class="card text-center justify-content-center m-4 p-0">
+        <a href="listing/?id=${listing.id}" class="listing-link">
+        <h2 class="card-header m-0">${listing.title}</h2>
+        <p class="listing-text my-3">${listing.description}</p>
         ${image}
-        <p class="listing-text m-2">${listing.description}</p>
         <p>${listing.tags}</p>
-        <p>${listing.endsAt}</p>
-        </div>
+        <p class="card-footer text-muted m-0">${listing.endsAt}</p>
         </a>
+        </div>
         `;
   }
 }
